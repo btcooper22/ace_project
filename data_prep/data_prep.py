77db646c3c8771bed1029ef9cf27a7606c9f7530
Sam Relins <samrelins@gmail.com>
2020-11-02 15:40:22 +0000

    first attempts at pipelining data prep and modeling

--- data_prep/data_prep.py
+++ data_prep/data_prep.py
@@ -6,8 +6,11 @@ from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
 from category_encoders.leave_one_out import LeaveOneOutEncoder
 
 
-def return_prepped_ace_data(loc):
-    ace_data = pd.read_csv(loc)
+def start_pipeline(dataf):
+    return dataf.copy()
+
+
+def clean_data(dataf):
 
     # check columns are present and named correctly
     reqd_features = ['hospital_reqd', 'referral_from', 'age', 'address',
@@ -15,11 +18,10 @@ def return_prepped_ace_data(loc):
                      'referral_time', 'illness_severity', 'activity_level',
                      'gut_feeling', 'ox_sat', 'resp_rate', 'heart_rate',
                      'temp', 'sepsis', 'safeguarding']
-
-    if sorted(ace_data.columns) != sorted(reqd_features):
+    if sorted(dataf.columns) != sorted(reqd_features):
         missing_features = [feature for feature in reqd_features
-                            if feature not in ace_data.columns]
-        extra_features = [feature for feature in ace_data.columns
+                            if feature not in dataf.columns]
+        extra_features = [feature for feature in dataf.columns
                           if feature not in reqd_features]
         raise ValueError(
             f"Input data should contain the folowing columns:\n{missing_features}"
@@ -27,44 +29,49 @@ def return_prepped_ace_data(loc):
         )
 
     # replace "None" values with nan
-    ace_data.replace("None", np.nan, inplace=True)
+    dataf.replace("None", np.nan, inplace=True)
 
     # set numeric features to float after removing "None" values
     float_features = ["ox_sat", "resp_rate", "heart_rate", "temp"]
     for feature in float_features:
-        ace_data[feature] = ace_data[feature].astype("float")
+        dataf[feature] = dataf[feature].astype("float")
 
     # set all referral from A&E to ED (same meaning)
-    ane_mask = ace_data.referral_from == "A&E"
-    ace_data.loc[ane_mask, "referral_from"] = "ED"
+    ane_mask = dataf.referral_from == "A&E"
+    dataf.loc[ane_mask, "referral_from"] = "ED"
 
     # clean and convert cat features to categorical datatype
     # (other than allergies - treated separately)
     cat_features = ['referral_from', 'address', 'ethnicity', 'gender',
                     'referral_date', 'referral_time', 'illness_severity',
                     'activity_level', 'gut_feeling', 'sepsis', 'safeguarding']
-    for feature in cat_features:
-        ace_data[feature] = (ace_data[feature].str.strip()
-                             .astype("category"))
 
+    for feature in cat_features:
+        dataf[feature] = (dataf[feature].str.strip()
+                          .astype("category"))
+
+    return dataf
+
+
+def add_features(dataf):
     # refactor allergy feature
     for allergy in ["Food", "Drug", "Other"]:
         allergy_name = allergy.lower() + "_allergy"
-        ace_data[allergy_name] = ace_data.allergies.apply(
+        dataf[allergy_name] = dataf.allergies.apply(
             lambda x: 'Y' if allergy in x else 'N'
         ).astype("category")
-    ace_data.drop("allergies", axis=1, inplace=True)
+    dataf.drop("allergies", axis=1, inplace=True)
 
     # set hospital_reqd to 1/0 for data analysis
-    ace_data["hospital_reqd"] = (ace_data.hospital_reqd == "Y").astype("int")
+    dataf["hospital_reqd"] = (dataf.hospital_reqd == "Y").astype("int")
 
     # new ethnicity features
     # set reported ethnicity to other if not British / Pakistani
-    ace_data["simple_ethnicity"] = ace_data.ethnicity.apply(
+    dataf["simple_ethnicity"] = dataf.ethnicity.apply(
         lambda x: x if x in ["Pakistani", "British"] else "other"
     ).replace(np.nan, "other")
 
-    ace_data["simple_ethnicity"] = ace_data.simple_ethnicity.astype("category")
+    dataf["simple_ethnicity"] = dataf.simple_ethnicity.astype("category")
 
     # Group ethnicities into European / Asian / Other
     asian = ["Indian", "Mixed Asian", "Pakistani", "white Asain", "British Asian",
@@ -74,29 +81,29 @@ def return_prepped_ace_data(loc):
                 "White Europeon", "White British", "CommonWealth Russian",
                 "Other European", "Mixed White"]
 
-    ace_data["group_ethnicity"] = "other"
-    ace_data.loc[ace_data.ethnicity.isin(european), "group_ethnicity"] = "european"
-    ace_data.loc[ace_data.ethnicity.isin(asian), "group_ethnicity"] = "asian"
+    dataf["group_ethnicity"] = "other"
+    dataf.loc[dataf.ethnicity.isin(european), "group_ethnicity"] = "european"
+    dataf.loc[dataf.ethnicity.isin(asian), "group_ethnicity"] = "asian"
 
-    ace_data["group_ethnicity"] = ace_data.group_ethnicity.astype("category")
-    ace_data.drop("ethnicity", axis=1, inplace=True)
+    dataf["group_ethnicity"] = dataf.group_ethnicity.astype("category")
+    dataf.drop("ethnicity", axis=1, inplace=True)
 
     # add features from ace referral sheets
     # ox_sat_low feature: < 94 = low
-    ace_data["ox_sat_low"] = ace_data.ox_sat.apply(
+    dataf["ox_sat_low"] = dataf.ox_sat.apply(
         lambda x: "Y" if x < 94 else "N"
     ).astype("category")
 
     # age_range feature: "pre_school" = (2-5), "primary" = (5-12), "secondary" (12+)
-    ace_data["age_range"] = "pre_school"
+    dataf["age_range"] = "pre_school"
 
-    primary = (ace_data.age >= 5) & (ace_data.age < 12)
-    ace_data.loc[primary, "age_range"] = "primary"
+    primary = (dataf.age >= 5) & (dataf.age < 12)
+    dataf.loc[primary, "age_range"] = "primary"
 
-    secondary = ace_data.age >= 12
-    ace_data.loc[secondary, "age_range"] = "secondary"
+    secondary = dataf.age >= 12
+    dataf.loc[secondary, "age_range"] = "secondary"
 
-    ace_data["age_range"] = ace_data.age_range.astype("category")
+    dataf["age_range"] = dataf.age_range.astype("category")
 
     # set low / normal / high ranges for ace / apls heart / resp rate
     # see ace & apls referral criteria for more info
@@ -104,16 +111,16 @@ def return_prepped_ace_data(loc):
     # generic function to set low / normal / high ranges for a feature within age ranges
     def set_low_norm_high(feature, feature_range, age_range, new_feature_name):
 
-        if new_feature_name not in ace_data.columns:
-            ace_data[new_feature_name] = "normal"
+        if new_feature_name not in dataf.columns:
+            dataf[new_feature_name] = "normal"
 
-        age_mask = ((ace_data.age >= age_range[0]) &
-                    (ace_data.age < age_range[1]))
-        low_mask = age_mask & (ace_data[feature] < feature_range[0])
-        high_mask = age_mask & (ace_data[feature] > feature_range[1])
+        age_mask = ((dataf.age >= age_range[0]) &
+                    (dataf.age < age_range[1]))
+        low_mask = age_mask & (dataf[feature] < feature_range[0])
+        high_mask = age_mask & (dataf[feature] > feature_range[1])
 
-        ace_data.loc[low_mask, new_feature_name] = "low"
-        ace_data.loc[high_mask, new_feature_name] = "high"
+        dataf.loc[low_mask, new_feature_name] = "low"
+        dataf.loc[high_mask, new_feature_name] = "high"
 
     # parameters for different apls / ace criteria
     ace_resp_rate_feature = {
@@ -166,21 +173,21 @@ def return_prepped_ace_data(loc):
                               age_range=age_range,
                               new_feature_name=new_feature_name)
 
-        ace_data[new_feature_name] = ace_data[new_feature_name].astype("category")
+        dataf[new_feature_name] = dataf[new_feature_name].astype("category")
 
     # overall feature for meeting ace referral criteria
-    ace_data["meets_ace_criteria"] = "N"
+    dataf["meets_ace_criteria"] = "N"
 
-    meets_ace_criteria = ((ace_data.ox_sat_low == "N") &
-                          (ace_data.ace_heart_rate_cat == "normal") &
-                          (ace_data.ace_resp_rate_cat == "normal") &
-                          (ace_data.gut_feeling != "unwell") &
-                          (ace_data.illness_severity != "Moderate"))
+    meets_ace_criteria = ((dataf.ox_sat_low == "N") &
+                          (dataf.ace_heart_rate_cat == "normal") &
+                          (dataf.ace_resp_rate_cat == "normal") &
+                          (dataf.gut_feeling != "unwell") &
+                          (dataf.illness_severity != "Moderate"))
 
-    ace_data.loc[meets_ace_criteria, "meets_ace_criteria"] = "Y"
-    ace_data["meets_ace_criteria"] = ace_data.meets_ace_criteria.astype("category")
+    dataf.loc[meets_ace_criteria, "meets_ace_criteria"] = "Y"
+    dataf["meets_ace_criteria"] = dataf.meets_ace_criteria.astype("category")
 
-    return ace_data
+    return dataf
 
 
 def return_train_test(loc, cat_encoder=None, resampled=False, scaled=False):
@@ -233,6 +240,9 @@ def return_train_test(loc, cat_encoder=None, resampled=False, scaled=False):
     X_test = X_test.reset_index(drop=True)
     y_test = y_test.reset_index(drop=True)
 
+    # copy original X_train / y_train for target encoding resampled data
+    X_train_orig, y_train_orig = X_train.copy(), y_train.copy()
+
     if resampled:
 
         cat_feature_idxs = []
@@ -242,9 +252,6 @@ def return_train_test(loc, cat_encoder=None, resampled=False, scaled=False):
 
         smote = SMOTENC(random_state=1,
                         categorical_features=cat_feature_idxs)
-
-        if cat_encoder == "target":
-            X_train_orig, y_train_orig = X_train.copy(), y_train.copy()
 
         X_train, y_train = smote.fit_resample(X_train, y_train)
         X_train = X_train.reset_index(drop=True)
@@ -265,6 +272,7 @@ def return_train_test(loc, cat_encoder=None, resampled=False, scaled=False):
                     name = feature + '_' + category
                     feature_names.append(name)
         elif cat_encoder == "target":
+
             encoder = LeaveOneOutEncoder().fit(X_train_orig[cat_features],
                                                y_train_orig)
             feature_names = cat_features

f11d888a6750e605ca37304ba4a9601c7a082054
Ben Cooper <ben.cooper.226@gmail.com>
2021-06-30 10:23:29 +0100

    Added vocabulary tables

--- scripts/functions/load_connected_yorkshire.R
+++ scripts/functions/load_connected_yorkshire.R
@@ -23,9 +23,6 @@ CY <- list()
 print(noquote("Loading Connected Yorkshire..."))
 for(i in 1:length(table_list))
 {
-  # Generate name
-  obj_name <- paste("CY", table_list[i], sep = "_")
-  
   # Pull table
   loaded_table <- tbl(con, table_list[i])
   
@@ -35,5 +32,37 @@ for(i in 1:length(table_list))
   # Rename
   names(CY)[i] <- table_list[i]
 }
-rm(i, obj_name, table_list, 
-   loaded_table, con)
+
+# Load vocabulary
+con2 <- dbConnect(
+  bigrquery::bigquery(),
+  project = "yhcr-prd-phm-bia-core",
+  dataset = "CY_CDM_VOCAB"
+)
+
+table_list2 <- bq_dataset_tables(con2) %>% unlist()
+table_list2 <- table_list2[seq(3, length(table_list2), by = 3)] %>% 
+  unname()
+
+# Create blank list
+CY_V <- list()
+
+print(noquote("Loading vocabulary..."))
+for(i in 1:length(table_list2))
+{
+  # Pull table
+  loaded_table <- tbl(con2, table_list2[i])
+  
+  # Insert into list
+  CY_V[[i]] <- loaded_table
+  
+  # Rename
+  names(CY_V)[i] <- paste("V", table_list2[i],
+                          sep = "_")
+}
+
+# Combine and cleanup
+CY <- c(CY, CY_V)
+
+rm(i, table_list, table_list2,
+   loaded_table, con, con2, CY_V)

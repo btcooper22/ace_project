4eca99212b4b9f098ff9a31c42ed6261a6288736
Ben Cooper <ben.cooper.226@gmail.com>
2021-07-07 12:08:14 +0100

    Built patient demographics query

---
+++ scripts/queries/prep/demographics.R
@@ -0,0 +1,50 @@
+# Packages
+require(dplyr)
+require(magrittr)
+require(readr)
+require(tibble)
+require(doParallel)
+
+# Load person_id list from ACE data
+person_id_list <- read_csv("data/ace_data_080621.csv",
+                           col_types = cols()) %>% 
+  select(pkid) %>% 
+  deframe() %>% 
+  unique() %>% 
+  na.omit() %>% 
+  as.numeric()
+
+# Load connected yorkshire
+source("scripts/functions/load_connected_yorkshire.R")
+
+# Pull out patients
+patients <- CY$person %>% 
+  filter(person_id %in% person_id_list) %>% 
+  select(person_id, gender_concept_id,
+         race_concept_id, ethnicity_concept_id,
+         gender_source_value, race_source_value,
+         ethnicity_source_value) %>%
+  collect()
+
+# Construct query
+demographics_query <- CY$person %>% 
+  filter(person_id %in% person_id_list) %>% 
+  select(person_id, gender_concept_id,
+         race_concept_id, ethnicity_concept_id,
+         gender_source_value, race_source_value,
+         ethnicity_source_value)
+
+# Test query (R)
+test_df <- demographics_query %>% 
+  collect()
+
+# (COPY + PASTE QUERY BELOW INTO SQL SCRIPT)
+show_query(demographics_query)
+
+# Load
+loaded_q <- read_file("scripts/queries/cooper_demographics_query.sql")
+
+# Test query (BQ)
+test_sql <- bq_dataset_query(bq_dataset("yhcr-prd-phm-bia-core", "CY_CDM_V1_50k_Random"),
+                             query = loaded_q, billing = "yhcr-prd-phm-bia-core")
+

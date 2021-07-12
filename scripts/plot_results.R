26c611a44eff0085e4fc387e380b8473cb447fa2
Ben Cooper <ben.cooper.226@gmail.com>
2021-07-12 19:33:09 +0100

    Added printing of summary table

--- scripts/plot_results.R
+++ scripts/plot_results.R
@@ -3,6 +3,7 @@ require(readr)
 require(ggplot2)
 require(dplyr)
 require(tidyr)
+require(forcats)
 
 # Load data
 results <- read_rds("analysis/boostrap_aggregate_additional.RDS") %>% 
@@ -16,7 +17,8 @@ results <- read_rds("analysis/boostrap_aggregate_additional.RDS") %>%
       pivot_longer(1:12) %>% 
       na.omit() %>% 
       mutate(model = "original")
-  )
+  ) %>% 
+  mutate(model = fct_rev(model))
 
 # Plot
 results %>% 
@@ -24,8 +26,21 @@ results %>%
   geom_density(aes(fill = model),
                alpha = 0.8)+
   scale_fill_brewer(palette = "Set1",
-                    name = "Model")+
+                    name = "Model",
+                    direction = -1)+
   theme_classic(20)+
   theme(legend.position = "top")+
   labs(x = "", y = "Density")+
   facet_wrap(~name, scales = "free")
+
+results %>% 
+  group_by(name, model) %>% 
+  summarise(median = round(median(value),2),
+            Q1 = round(quantile(value, 0.25),2),
+            Q3 = round(quantile(value, 0.75),2)) %>% 
+  mutate(value = paste(median, " [", Q1, ", ",
+                       Q3, "]", sep = "")) %>% 
+  select(metric = name, model, value) %>% 
+  pivot_wider(names_from = model,
+              values_from = value) %>% 
+  relocate(metric, original, additional)
